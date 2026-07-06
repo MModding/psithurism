7382f51fe3fd9490c585bf68d30ca801f91c088b
FirstMegaGame4 <84094287+FirstMegaGame4@users.noreply.github.com>
2026-07-07 00:02:14 +0200

    uh

--- src/main/java/com/mmodding/psithurism/resource/PsithurismJukeboxSoundResources.java
+++ src/main/java/com/mmodding/psithurism/resource/PsithurismJukeboxSoundResources.java
@@ -10,7 +10,7 @@ import net.minecraft.world.item.JukeboxSong;
 public class PsithurismJukeboxSoundResources {
 
 	public static void configure(AdvancedContainer mod, BootstrapContext<JukeboxSong> context) {
-		context.register(PsithurismJukeboxSounds.TALES_OF_YORE, new JukeboxSong(PsithurismSoundEvents.TALES_OF_YORE, Component.translatable("item.psithurism.music_disc_tales_of_yore.desc"), 73.014f, 6));
-		context.register(PsithurismJukeboxSounds.REST, new JukeboxSong(PsithurismSoundEvents.REST, Component.translatable("item.psithurism.music_disc_rest.desc"), 173.164f, 2));
+		context.register(PsithurismJukeboxSounds.TALES_OF_YORE, new JukeboxSong(PsithurismSoundEvents.TALES_OF_YORE, Component.translatable("item.psithurism.music_disc_tales_of_yore.desc"), 173.164f, 6));
+		context.register(PsithurismJukeboxSounds.REST, new JukeboxSong(PsithurismSoundEvents.REST, Component.translatable("item.psithurism.music_disc_rest.desc"), 73.014f, 2));
 	}
 }
